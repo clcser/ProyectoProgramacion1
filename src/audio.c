373c44a881ad8da50dd60f07b8d79c58b2f21173
Ana <anvillagran2021@inf.udec.cl>
2022-07-31 19:39:53 -0400

    Música de fondo en menú y juego

--- src/audio.c
+++ src/audio.c
@@ -7,11 +7,14 @@
 Music Music_new(){
 	Music music;
 	music.audio = Mix_LoadMUS("../audio/inGame.mp3");
+	music.start = Mix_LoadMUS("../audio/mainMenu.mp3");
 	return music;
 }
 
 void quit_Audio(Music music){
 	Mix_FreeMusic(music.audio);	//liberamos la musica
+	Mix_FreeMusic(music.start);
 	music.audio = NULL;
+	music.start = NULL;
 	Mix_Quit();	//Cerrar la musica
 }
--- src/main.c
+++ src/main.c
@@ -14,13 +14,18 @@
 int main(int argc, char *argv[]) {
     srand(time(0));
     Context_init();
+
+    Music music = Music_new();
+
     Start_menu start_menu = Start_menu_new();
-    
+    Mix_PlayMusic(music.start, -1);
     while(Start_menu_state(&start_menu)){   // PENDIENTE HACER QUE CON Q SE SALGA DE TODO EL JUEGO Y NO SÓLO DEL MENÚ
         Start_menu_draw(start_menu, start_menu.frame);
-    } 
+    }
+    Mix_HaltMusic();
      
     Game game = Game_new();
+    Mix_PlayMusic(music.audio, -1);
     while(Game_update_state(&game)) 
         Game_draw(game, game.costume, game.scenery);
     Game_delete(game);
